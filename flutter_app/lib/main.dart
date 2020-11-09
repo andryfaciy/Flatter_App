50c80fe1653c4c55c153f6ec760b873dd4e8300b
Kaat <bolgovn92@gmail.com>
2020-11-09 09:28:15 +0300

    Update main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,117 +1,187 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('История'),
+      ),
+      drawer: new Drawer(
+          child: new ListView(
+            children: <Widget>[
+              new AppBar(
+                title: new Text('Меню'),
+              ),
+              ButtonTheme(
+                height: 60,
+                child: RaisedButton(
+                  onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => MyHomePage()));},
+                  child: Text('История', style: TextStyle(fontSize: 15)),
+                  color: Colors.white,
+                ),
+              ),
+              ButtonTheme(
+                height: 60,
+                child: RaisedButton(
+                  onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => TemplatesPage()));},
+                  child: Text('Шаблоны', style: TextStyle(fontSize: 15)),
+                  color: Colors.white,
+                ),
+              ),
+              ButtonTheme(
+                height: 60,
+                child: RaisedButton(
+                  onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => ReportsPage()));},
+                  child: Text('Отчеты', style: TextStyle(fontSize: 15)),
+                  color: Colors.white,
+                ),
+              ),
+              ButtonTheme(
+                height: 60,
+                child: RaisedButton(
+                  onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => SettingsPage()));},
+                  child: Text('Настройки', style: TextStyle(fontSize: 15)),
+                  color: Colors.white,
+                ),
+              ),
+              ButtonTheme(
+                  height: 60,
+                  child: RaisedButton(
+                    onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => WalletsPage()));},
+                    child: Text('Кошельки', style: TextStyle(fontSize: 15)),
+                    color: Colors.white,
+                  ))
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+            ],
+          )),
+    );
+  }
+}
+
+class SettingsPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => new _TabsPageState();
+}
+
+class _TabsPageState extends State<SettingsPage>{
+  bool isInstructionView;
+  @override
+  void initState() {
+    isInstructionView = Global.shared.isInstructionView;
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: new AppBar(
+        title: new Text("Настройки"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: new Container( alignment: FractionalOffset(0.95, 0.02),
+
+        child:  SwitchListTile(
+          title: Text("Тёмная тема"),
+          value: isInstructionView,
+          onChanged: (bool isOn) {
+            print(isOn);
+            setState(() {
+              isInstructionView = isOn;
+              Global.shared.isInstructionView = isOn;
+              isOn =!isOn;
+              print(isInstructionView);
+            });
+          },
+          activeColor: Colors.green,
+          inactiveTrackColor: Colors.red,
+          inactiveThumbColor: Colors.red,
+
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+    );
+  }
+}
+
+class Global{
+  static final shared =Global();
+  bool isInstructionView = false;
+}
+class TemplatesPage extends StatelessWidget {   //Экран Шаблоны
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Шаблоны'),),
+
+    );
+  }
+}
+class WalletsPage extends StatelessWidget {   //Экран кошельков
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Кошельки')),
+        body: BodyLayout(),
+      ),
+    );
+  }
+}
+class BodyLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView(context);
+  }
+}
+Widget _myListView(BuildContext context) {
+  return ListView(
+    children: ListTile.divideTiles(
+      context: context,
+      tiles: [
+        ListTile(
+          title: Text('Кошелек 1'),
+        ),
+        ListTile(
+          title: Text('Кошелек 2'),
+        ),
+        ListTile(
+          title: Text('Кошелек 3'),
+        ),
+      ],
+    ).toList(),
+  );
+}
+
+
+//Болгов лох
+class ReportsPage extends StatelessWidget {   //Экран отчетов
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Отчеты'),),
+
     );
   }
 }
