9161cbeb1bbbc8eef75ef03336829d50e9b585b8
Drew Kozlov <andreigbk@gmail.com>
2020-11-15 13:56:53 +0300

    Andryfaciy
    Болгов, учи теорию! исправил переключалки в настройках и вернул кнопку назад в кошельках.Только попробуй что нибудь снова сломать,я сломаю тебя. с любовью Андрей

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo',debugShowCheckedModeBanner: false,
       home: new MyHomePage(),
     );
   }
@@ -86,9 +86,11 @@ class SettingsPage extends StatefulWidget {
 
 class _TabsPageState extends State<SettingsPage>{
   bool isInstructionView;
+  bool isInstructionTheme;
   @override
   void initState() {
     isInstructionView = Global.shared.isInstructionView;
+    isInstructionTheme = Global.shared.isInstructionView;
     super.initState();
   }
 
@@ -98,25 +100,81 @@ class _TabsPageState extends State<SettingsPage>{
       appBar: new AppBar(
         title: new Text("Настройки"),
       ),
-      body: new Container( alignment: FractionalOffset(0.95, 0.02),
+      body: Column(
+          children:[
+            new Container( alignment: FractionalOffset(0.95, 0.02),
 
-        child:  SwitchListTile(
-          title: Text("Тёмная тема"),
-          value: isInstructionView,
-          onChanged: (bool isOn) {
-            print(isOn);
-            setState(() {
-              isInstructionView = isOn;
-              Global.shared.isInstructionView = isOn;
-              isOn =!isOn;
-              print(isInstructionView);
-            });
-          },
-          activeColor: Colors.green,
-          inactiveTrackColor: Colors.red,
-          inactiveThumbColor: Colors.red,
+              child:  SwitchListTile(
+                title: Text("Тёмная тема"),
+                value: isInstructionTheme,
+                onChanged: (bool isOn) {
+                  print(isOn);
+                  setState(() {
+                    isInstructionTheme = isOn;
+                    Global.shared.isInstructionView = isOn;
+                    isOn =!isOn;
+                    print(isInstructionTheme);
+                  });
+                },
+                activeColor: Colors.green,
+                inactiveTrackColor: Colors.red,
+                inactiveThumbColor: Colors.red,
+              ),
+            ),
+            new Container( alignment: FractionalOffset(0.95, 0.02),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 0.05,
+                  )
+              ),
 
-        ),
+              child:  SwitchListTile(
+                title: Text("Уведомления"),
+                value: isInstructionView,
+                onChanged: (bool isOn) {
+                  print(isOn);
+                  setState(() {
+                    isInstructionView = isOn;
+                    Global.shared.isInstructionView = isOn;
+                    isOn =!isOn;
+                    print(isInstructionView);
+                  });
+                },
+                activeColor: Colors.green,
+                inactiveTrackColor: Colors.red,
+                inactiveThumbColor: Colors.red,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print("Tapped a Container");
+              },
+              child: Container(
+                height: 60,
+                width: MediaQuery.of(context).size.width,
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 0.05,
+                    )
+                ),
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                  child: Text("Даниил привет",style: TextStyle(fontSize: 16), textAlign: TextAlign.left),),
+              ),
+            ),
+            new Container(
+              height: 60,
+              padding: EdgeInsets.all(20),
+              child: new Text("Выбранная валюта" , style: TextStyle(fontSize: 16)),
+              alignment: Alignment.topLeft,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 0.05,
+                  )
+              ),
+            ),
+          ]
       ),
 
     );
@@ -127,45 +185,66 @@ class Global{
   static final shared =Global();
   bool isInstructionView = false;
 }
+
 class TemplatesPage extends StatelessWidget {   //Экран Шаблоны
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Шаблоны'),),
-
-    );
-  }
-}
-class WalletsPage extends StatelessWidget {   //Экран кошельков
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Кошельки'),),
-      body: Column(
-        children :[
-          ButtonTheme(
-              height: 61, minWidth: 360,
-
-              child: RaisedButton(
-                child: Text('Кошелек 0', style: TextStyle(fontSize: 20)),
-                color: Colors.white,
-                onPressed: () {},
-              )
-          ),
-          ButtonTheme(
-              height: 60, minWidth: 360,
-              child: RaisedButton(
-                child: new Text("Кошелек 1" , style: TextStyle(fontSize: 15)),
-                color: Colors.white,
-                onPressed: () {},
-              )
-          ),
-        ],
+      appBar: AppBar(title: Text('Шаблоны'),leading: IconButton(
+        icon: Icon(
+          Icons.arrow_back,
+        ),
+        onPressed:() => Navigator.pop(context, false),),
       ),
     );
   }
 }
-//Болгов лох
+
+class WalletsPage extends StatelessWidget {   //Экран кошельков
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Кошельки'),leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+          ),
+          onPressed:() => Navigator.pop(context, false),),),
+        body: BodyLayout(),
+      ),
+    );
+  }
+}
+
+class BodyLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return _myListView(context);
+  }
+}
+
+Widget _myListView(BuildContext context) {
+  return ListView(
+    children: ListTile.divideTiles(
+      context: context,
+      tiles: [
+        ListTile(
+          title: Text('Кошелек 1'),
+        ),
+        ListTile(
+          title: Text('Кошелек 2'),
+        ),
+        ListTile(
+          title: Text('Кошелек 3'),
+        ),
+      ],
+    ).toList(),
+  );
+}
+
 class ReportsPage extends StatelessWidget {   //Экран отчетов
   Widget build(BuildContext context) {
     return Scaffold(
